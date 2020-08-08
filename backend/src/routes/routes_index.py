985985d17063aa31bb8def178c349f03cb927107
HyperTech99 <smyun99@gmail.com>
2020-08-09 02:08:38 +0900

    Implement failback route

--- backend/src/routes/routes_index.py
+++ backend/src/routes/routes_index.py
@@ -2,11 +2,8 @@ from .. import app
 from flask import render_template, url_for
 
 # SPA Template 페이지
-@app.route("/")
-def home():
+@app.route("/", defaults={"path": ""})
+@app.route("/<string:path>")
+@app.route("/<path:path>")
+def home(path):
   return render_template("index.html")
-
-# SPA Virtual Router 대응 (FailBack Route를 index.html로 설정)
-@app.errorhandler(404)
-def failback():
-  home()
